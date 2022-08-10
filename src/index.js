cddeffa76e8e098f61c704bc0f7d069d45fd29ad
GustavoArcoverde <garcoverde3@gmail.com>
2022-08-10 14:14:24 -0300

    altera listen

--- src/index.js
+++ src/index.js
@@ -9,5 +9,8 @@ app.use(cors())
 
 cliente(app, bdSQLite)
 
-const port = process.env.PORT || 3030;
-app.listen(port, ()=>console.log(`http://localhost:${port}`))
+const port = process.env.PORT || 3030
+// app.listen(port, ()=>console.log(`http://localhost:${port}`))
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+    });
